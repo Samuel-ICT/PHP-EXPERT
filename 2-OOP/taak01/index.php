05589118ffa47d728f4e8d8c2dbac8f8f076a46d
Samuel Adu <samuel.adu@student.rocva.nl>
2021-09-20 11:11:40 +0200

    02 taak 1

---
+++ 2-OOP/taak01/index.php
@@ -0,0 +1,13 @@
+<?php
+class Auto{
+    public $merk;
+    public $kleur;
+    public $type;
+    public $uitvoering;
+    public $brandstof;
+}
+
+$nieuweAuto = new Auto();
+$nogEenAndereAuto = new Auto();
+
+?>
