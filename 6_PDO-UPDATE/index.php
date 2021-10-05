6caac14e42ae6f0cade028f7bf2f2ba5bc7558f7
Samuel Adu <samuel.adu@student.rocva.nl>
2021-10-05 13:58:29 +0200

    06 taak 1

--- 6_PDO-UPDATE/index.php
+++ 6_PDO-UPDATE/index.php
@@ -1,17 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="POST">
-    Voornaam:<input type="text" name = "firstname">
-    </form>
-</body>
-</html> -->
 <?php
 $database_lokaal = 'localhost';
 $database_naam = 'toolsforever';
