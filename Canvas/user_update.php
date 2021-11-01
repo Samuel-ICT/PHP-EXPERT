50437e3c2b09763c428960471674943a0af8a1e2
Samuel Adu <samuel.adu@student.rocva.nl>
2021-11-01 15:57:00 +0100

    user_update

---
+++ Canvas/user_update.php
@@ -0,0 +1,18 @@
+<?php
+
+$database_lokaal = 'localhost';
+$database_naam = 'toolsforever';
+$database_gebruiker = 'root';
+$database_wachtwoord = '';
+
+$database_connectie = new PDO("mysql:host=$database_lokaal;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
+
+$firstname = 'Mohammed';
+$id = 1;
+$sql = 'UPDATE users SET firstname = :firstname WHERE id = :id';
+$statement = $database_connectie->prepare($sql);
+$statement->bindParam(':firstname', $firstname);
+$statement->bindParam(':id', $id);
+$statement->execute();
+
+?>
